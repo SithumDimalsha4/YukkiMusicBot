1b660c635555762be448604cb44c9749bbdc1ce4
SithumDimalsha4 <97735163+SithumDimalsha4@users.noreply.github.com>
2022-05-01 20:18:42 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -33,7 +33,7 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
+                config.LOG_GROUP_ID, "DaisyX Music Bot has started successfully!💫"
             )
         except:
             LOGGER(__name__).error(
